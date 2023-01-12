1aec149b4e3f7e20ecfb2699460a709056b85e61
JIAWEI68 <ang.jiawei56@gmail.com>
2023-01-13 00:30:04 +0800

    Finished with the designing of the navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,33 +1,97 @@
 //import index.css
-import "./Navbar.css";
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Spacer,
+  Heading,
+  Input,
+  LinkOverlay,
+  Breadcrumb,
+  BreadcrumbLink,
+  BreadcrumbItem,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+
+const Links = ["Login", "Signup"];
 
 const Navbar = () => {
   return (
-    <div className="navigation">
-      <Link to="/" className="brand-name">
-        Photo4You
-      </Link>
-      <input
-        className="search-bar"
-        id="searchQueryInput"
-        type="text"
-        name="searchQueryInput"
-        placeholder="Search"
-        value=""
-      />
-      <div className="navigation-menu">
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Sign Up</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <Box bg='#00C65A' w='2131px' mt='-10' ml='-8'>
+    <Flex minWidth="max-content" alignItems="center" gap="2" color="grey">
+      <Box p="2">
+        <Heading size="md" ml= '24'>
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              fontFamily: "Raleway",
+              color: "black",
+            }}
+          >
+            Photo4You
+          </Link>
+        </Heading>
+      </Box>
+      <Spacer />
+      <Box>
+        <Input
+          placeholder="Search"
+          fontSize= '20'
+          style={{
+            textDecoration: "none",
+            textAlign: "center",
+            borderRadius: "1.625rem",
+            width: "344px",
+            height: "38px",
+          }}
+          size="lg"
+        />
+      </Box>
+      <Spacer />
+      <Breadcrumb mr = '100'>
+        <BreadcrumbItem>
+          <BreadcrumbLink
+            as={Link}
+            to="/login"
+            style={{
+              textDecoration: "none",
+              fontFamily: "Raleway",
+              color: "black",
+            }}
+          >
+            Login
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink
+            as={Link}
+            to="/signup"
+            style={{
+              textDecoration: "none",
+              fontFamily: "Raleway",
+              color: "black",
+            }}
+          >
+            Signup
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+    </Flex>
+    </Box>
   );
 };
 
 export default Navbar;
+
