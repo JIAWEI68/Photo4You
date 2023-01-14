4954d2eb5a882ea283a27b3fd649d3e36bad43ae
JIAWEI68 <ang.jiawei56@gmail.com>
2023-01-14 17:29:01 +0800

    Started on designing for the posts

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Box, Card, SimpleGrid } from "@chakra-ui/react";
+import React, { useEffect } from "react";
 
-const Home = () => {
+function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    //useEffect is a hook that runs after every render
+    fetch(
+      "http://localhost:5000/posts",
+      {
+        //fetch data from the server
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      []
+    );
+  });
   return (
-    <div>Home</div>
-  )
+  <div class = "container">
+    <SimpleGrid minChildWidth='120px' spacing = '40px'>
+      <Box maxW = 'sm' borderWidth='1px' borderRadius='lg'>
+        <Box display='flex' alignItems = 'stretch'>
+
+        </Box>
+      </Box>
+    </SimpleGrid>
+    
+  </div>
+  );
 }
 
-export default Home
+export default Home;
