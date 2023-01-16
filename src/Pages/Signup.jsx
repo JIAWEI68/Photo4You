76dc8119bb633193527a6256ea56ca89b268b644
JIAWEI68 <ang.jiawei56@gmail.com>
2023-01-16 14:04:05 +0800

    Created Sign in page

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <Box>
+      <Box>
+        <Center>
+          <Box borderWidth="1px">
+            <Headers>Login</Headers>
+            <Box p="2">
+              <Input placeholder="Usernmae" size="lg" />
+            </Box>
+            <Box p="2">
+              <Input placeholder="Email" size="lg" />
+            </Box>
+            <Box p="2">
+              <Input placeholder="Password" size="lg" />
+            </Box>
+            <Box p="2">
+              <Input placeholder="Confirm Password" size="lg" />
+            </Box>
+            <Box p="1">
+              Already have an account? <Link to = '/login'>Login</Link>
+            </Box>
+            <Button p="5">Sign Up</Button>
+          </Box>
+        </Center>
+      </Box>
+    </Box>
+  );
+};
 
-export default Signup
+export default Signup;
