6fb03df454659ec64b5c707bbf426512db9d0011
JIAWEI68 <ang.jiawei56@gmail.com>
2023-01-22 14:40:15 +0800

    started on create posts

--- src/Pages/CreatePost.jsx
+++ src/Pages/CreatePost.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Input, InputGroup, InputLeftAddon } from "@chakra-ui/react";
+import React from "react";
 
 const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
-}
+  const [title, setTitle] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [author, setAuthor] = React.useState("");
+  const [image, setImage] = React.useState("");
+  const [type, setType] = React.useState("");
 
-export default CreatePost
+  function createPosts() {
+    fetch("http://localhost:5000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        description: description,
+        author: author,
+        image: image,
+        type: type,
+      }),
+    });
+  }
+  
+  return (
+    <div>
+      <InputGroup>
+        <InputLeftAddon children="Title" />
+        <Input />
+      </InputGroup>
+    </div>
+  );
+};
+
+export default CreatePost;
