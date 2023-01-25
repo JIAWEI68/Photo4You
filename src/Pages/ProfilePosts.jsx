642cbc6811578b150b09906b5ae70fdb91006620
JIAWEI68 <ang.jiawei56@gmail.com>
2023-01-26 03:52:14 +0800

    Added posts for profile page

--- src/Pages/ProfilePosts.jsx
+++ src/Pages/ProfilePosts.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import { useState } from 'react'
 
 const ProfilePosts = () => {
+  const [userId, setUserId] = useState("")
+  const [postsList, setPosts] = useState([])
+  const id = sessionStorage.getItem("userId")
+  useEffect(() => {
+    fetch(`http://localhost:3000/posts/${id}`)
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
   return (
-    <div>PostsPage</div>
+    <div className="container">
+      <Center>
+        <Box
+          borderWidth="2px"
+          width="100px"
+          height="30px"
+          mt="5"
+          borderRadius="10"
+          backgroundColor="#00C65A"
+        >
+          <Center>
+            <Link
+              to="/saves"
+              style={{ textDecoration: "none", fontFamily: "Raleway" }}
+            >
+              Saved
+            </Link>
+          </Center>
+        </Box>
+      </Center>
+      <SimpleGrid minChildWidth="120px" spacing="40px" mt="10">
+        {postsList.map((post) => (
+          <Box maxW="sm" borderWidth="1px" borderRadius="lg" key = {post.id}>
+            <Image src={post.image} />
+            <Box p="6">
+              <Box display="flex" alignItems="stretch" overflow="hidden">
+                <Box mt="1" font="Raleway">
+                  {post.title}
+                </Box>
+              </Box>
+            </Box>
+            <Box p="6">
+              <Button onClick={openModal(post)}>Details</Button>
+            </Box>
+          </Box>
+        ))}
+      </SimpleGrid>
+
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <PostsModal props={currentPost} />
+      </Modal>
+    </div>
   )
 }
 
