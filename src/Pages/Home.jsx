a38e3490fe7ffbc39040770201cbe42ce024b178
JIAWEI68 <ang.jiawei56@gmail.com>
2023-01-27 10:48:50 +0800

    Fixed getting data, now modal has bugs

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -23,24 +23,31 @@ function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [selected, setSelected] = useState(null);
   const [currentPost, setCurrentPost] = useState();
-  const fetchData = async() => {
-    return fetch("https://fejpqh9rn7.execute-api.us-east-1.amazonaws.com/posts").then((response) =>
-      setPosts(response.json())
-    );
+  const fetchData = async () => {
+    try {
+      const result = await fetch(
+        "https://fejpqh9rn7.execute-api.us-east-1.amazonaws.com/posts"
+      );
+      const data = await result.json();
+
+      setPosts(data);
+      console.log(postsList);
+    } catch (error) {
+      console.log(error);
+    }
   };
-  let postsL = Array.from(postsList)
+  let postsL = Array.from(postsList);
   useEffect(() => {
     //useEffect is a hook that runs after every render
     fetchData();
-    console.log(postsList)
   }, []);
   const onClick = () => {
     setSelected("");
   };
-  function openModal(post){
+  function openModal(post) {
     onOpen;
     setCurrentPost(post);
-  };
+  }
 
   return (
     <div className="container">
@@ -64,21 +71,22 @@ function Home() {
         </Box>
       </Center>
       <SimpleGrid minChildWidth="120px" spacing="40px" mt="10">
-        { postsList.length > 0 && postsList.map((post) => (
-          <Box maxW="sm" borderWidth="1px" borderRadius="lg" key = {post.id}>
-            <Image src={post.image} />
-            <Box p="6">
-              <Box display="flex" alignItems="stretch" overflow="hidden">
-                <Box mt="1" font="Raleway">
-                  {post.title}
+        {postsList.length > 0 &&
+          postsList.map((post) => (
+            <Box maxW="sm" borderWidth="1px" borderRadius="lg" key={post.id}>
+              <Image src={post.image} />
+              <Box p="6">
+                <Box display="flex" alignItems="stretch" overflow="hidden">
+                  <Box mt="1" font="Raleway">
+                    {post.title}
+                  </Box>
                 </Box>
               </Box>
+              <Box p="6">
+                <Button onClick={(post) => openModal(post)}>Details</Button>
+              </Box>
             </Box>
-            <Box p="6">
-              <Button onClick={openModal(post)}>Details</Button>
-            </Box>
-          </Box>
-        ))}
+          ))}
       </SimpleGrid>
 
       <Modal onClose={onClose} isOpen={isOpen} isCentered>
